3ec40d1111d1f12ecb450947c7261e7989655e85
Souvik-code-ai <purkaitsouvik531@gmial.com>
2025-10-14 18:15:53 +0530

    .\filtering_of_number_from_array_of_number_&_string.js

---
+++ filtering_of_number_from_array_of_number_&_string.js
@@ -0,0 +1,25 @@
+/*
+Filter out Strings from an Array
+Create a function that takes an array of non-negative integers and strings and return a new array without the strings.
+Examples
+filterArray([1, 2, "a", "b"]) ➞ [1, 2]
+filterArray([1, "a", "b", 0, 15]) ➞ [1, 0, 15]
+filterArray([1, 2, "aasf", "1", "123", 123]) ➞ [1, 2, 123]
+Notes
+Zero is a non-negative integer.
+The given array only has integers and strings.
+Numbers in the array should not repeat.
+The original order must be maintained.*/
+var array=[1, 2, "aasf", "1", "123", 123];
+function makingArrayOfIntezars(array){
+    let arrayOfIntezars=[];
+    for(let i=0;i<array.length;i++){
+        if((typeof array[i])==="number"){
+            arrayOfIntezars.push(array[i]);
+
+        }
+
+    }
+    return arrayOfIntezars;
+}
+console.log(makingArrayOfIntezars(array));
