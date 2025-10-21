7c46c1a9ccae87820b84f9b1474b82a574e35633
Souvik-code-ai <purkaitsouvik531@gmial.com>
2025-10-21 13:40:56 +0530

    .\star_pattern_8.js

---
+++ js_21st_oct/star_pattern_21st_Oct/star_pattern_8.js
@@ -0,0 +1,23 @@
+/*Star Pattern 8*/
+function star_pattern_7(N){
+    let j;
+    for(let i=1;i<=N;i++){
+        let row="";
+        for(j=1;j<=N;j++){
+            if(j<i){
+                row+=" ";
+            }
+            else if(j>=i){
+                row+="*";
+            }
+        }
+        if((i<N)&&(j>N)){
+            for(let k=(N+1);k<=((2*N)-i);k++){
+                row+="*";
+            }
+        }
+        
+        console.log(row);
+    }
+}
+star_pattern_7(5);
