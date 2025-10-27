679f33ca417a1977afebdf8d31a409cd2adff15b
Souvik-code-ai <purkaitsouvik531@gmial.com>
2025-10-27 15:06:33 +0530

    .\return_no_of_true_in_array.js

---
+++ js_27th_oct/return_no_of_true_in_array.js
@@ -0,0 +1,23 @@
+// How Much is True?
+// Create a function which returns the number of true values there are in an array.
+
+// Examples
+// countTrue([true, false, false, true, false]) ➞ 2
+
+// countTrue([false, false, false, false]) ➞ 0
+
+// countTrue([]) ➞ 0
+// Notes
+// Return 0 if given an empty array.
+// All array items are of the type bool (true or false).
+let array=[false, false, false, false];
+function countTrue(array){
+    let no_of_true=0;
+    for(let i=0;i<array.length;i++){
+        if(array[i]===true){
+            no_of_true++;
+        }
+    }
+    return no_of_true;
+}
+console.log(countTrue(array));
