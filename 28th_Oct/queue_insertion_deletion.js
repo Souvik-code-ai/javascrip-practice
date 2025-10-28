c4f895717f6c073d4671da91960e2b396cfaabda
Souvik-code-ai <purkaitsouvik531@gmial.com>
2025-10-28 15:49:17 +0530

    Queue operation

---
+++ 28th_Oct/queue_insertion_deletion.js
@@ -0,0 +1,34 @@
+class Queue {
+  constructor() {
+    this.queue = [];
+  }
+  enqueue(data) {
+    this.queue.push(data);
+  }
+  dequeue() 
+  {
+    return this.queue.shift();
+  }
+	printQueue()
+	{
+    let str = "";
+    for (let i = 0; i < this.queue.length; i++)
+        str += this.queue[i] + " ";
+    return str;
+	}
+}
+
+//create an object of type queue
+let myQueue = new Queue();
+
+//insert items into the queue
+myQueue.enqueue(2);
+myQueue.enqueue(3);
+myQueue.enqueue(9);
+myQueue.enqueue(1);
+
+//print the queue
+console.log("Your queue is:\n"+myQueue.printQueue());
+
+myQueue.dequeue();
+console.log("Your queue after deletion is:\n"+myQueue.printQueue());
